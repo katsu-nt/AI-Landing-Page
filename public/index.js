e070bbc18f270d4cec2201ea9548a55c27ccfadd
Admin <benbilldev@gmail.com>
2024-12-08 09:52:35 +0700

    Update new UI

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 //header click
-const navLinks = document.querySelectorAll('header nav ul li a');
+const navLinks = document.querySelectorAll('header nav ul li .nav-item');
 
 navLinks.forEach(link => {
     link.addEventListener('click', (event) => {
@@ -34,86 +34,93 @@ countUpNumbers()
 setInterval(countUpNumbers, 11000)
 
 //infinite loop
-// Number of items viewable at one time 
-var scrollerItemsViewable = 4;
-// Scroller speed in seconds
-var scrollerSpeed = 3;
-// Transition speed - must be <= scrollerSpeed
-var scrollerTransitionSpeed = 2;
+// Configurations
+const config = {
+  scrollerSpeed: 3, // seconds
+  scrollerTransitionSpeed: 2, // seconds, must be <= scrollerSpeed
+};
 
-// Rebuild the element list to remove white space
-var scrollerItemsEl = '';
-$('.scrollerContainer > article').each(function(index) {
-  //console.log( index + ": " + $( this ).text() );
-  scrollerItemsEl += $(this).prop('outerHTML');
-});
-$('.scrollerContainer').html(scrollerItemsEl);
-
-// Wrap items in a box that is as wide as the all the elements combined.
-// This prevents the items from wrapping if wider than the scroller width
-$('.scrollerContainer > article').wrapAll('<div class="scrollerGroup" />');
-var scrollerCount = $('.scrollerContainer .scrollerGroup > article').length;
-var scrollerItemWidth = parseInt($('.scrollerContainer').css('width')) / scrollerItemsViewable;
-$('.scrollerContainer .scrollerGroup > article').css('width', scrollerItemWidth + 'px');
-$('.scrollerContainer .scrollerGroup').css('width',scrollerCount * scrollerItemWidth + 'px');
-$('.scrollerContainer .scrollerGroup > article').css('transition', 'margin ' + scrollerTransitionSpeed + 's');
-
-// Set Starting Values
-var scrollerLeftMargin = '-' + scrollerItemWidth + 'px';
-var scrollerFirstItem = true;
-
-scrollerAnimate(scrollerSpeed);
-
-function scrollerAnimate(speed) {
-  setInterval(scrollerRotate, speed * 1000);
-}
-
-function scrollerRotate() {
-  if (scrollerFirstItem) {
-    scrollerFirstItem = false;
+function updateScrollerItemsViewable() {
+  if (window.innerWidth >= 1024) {
+    return 4;
   } else {
-    $('.scrollerContainer .scrollerGroup').append($('.scrollerContainer .scrollerGroup article:first-child'));
+    return 3;
   }
-  $('.scrollerContainer .scrollerGroup > article').css('margin-left', '0');
-  $('.scrollerContainer .scrollerGroup > article:first-child').css('margin-left', scrollerLeftMargin);
 }
 
+function initializeScroller(containerSelector, groupClass) {
+  const $container = $(containerSelector);
+  const $articles = $container.find('> article');
 
+  // Clone elements to enable infinite scroll
+  const scrollerItemsHTML = $articles.map((_, el) => $(el).prop('outerHTML')).get().join('');
+  $container.html(scrollerItemsHTML);
 
+  // Wrap items in a specific group class to avoid conflicts
+  $container.find('> article').wrapAll(`<div class="${groupClass}" />`);
+  const $scrollerGroup = $container.find(`.${groupClass}`);
 
-//loop-2
-var scrollerItemsEl01 = '';
-$('.scrollerContainer-02 > article').each(function(index) {
-  //console.log( index + ": " + $( this ).text() );
-  scrollerItemsEl01 += $(this).prop('outerHTML');
+  return { $container, $scrollerGroup };
+}
+
+function setupScroller(scroller, itemsViewable) {
+  const { $container, $scrollerGroup } = scroller;
+
+  const scrollerWidth = $container.width();
+  const itemWidth = scrollerWidth / itemsViewable;
+  const scrollerCount = $scrollerGroup.find('> article').length;
+
+  $scrollerGroup.find('> article').css({
+    width: `${itemWidth}px`,
+    transition: `margin ${config.scrollerTransitionSpeed}s`,
+  });
+  $scrollerGroup.css('width', `${scrollerCount * itemWidth}px`);
+
+  scroller.itemWidth = itemWidth;
+  scroller.leftMargin = -itemWidth;
+}
+
+function startScroller(scroller) {
+  const { $scrollerGroup } = scroller;
+
+  function rotate() {
+    const $first = $scrollerGroup.find('> article:first-child');
+    $first.css('margin-left', `${scroller.leftMargin}px`);
+
+    setTimeout(() => {
+      $first.css('margin-left', '0');
+      $scrollerGroup.append($first);
+    }, config.scrollerTransitionSpeed * 1000);
+  }
+
+  scroller.interval = setInterval(rotate, config.scrollerSpeed * 1000);
+}
+
+function resetScroller(scroller, itemsViewable) {
+  clearInterval(scroller.interval);
+  setupScroller(scroller, itemsViewable);
+  startScroller(scroller);
+}
+
+function initializeResponsiveScroller() {
+  const itemsViewable = updateScrollerItemsViewable();
+
+  resetScroller(scroller1, itemsViewable);
+  resetScroller(scroller2, itemsViewable);
+}
+
+// Initialize scrollers with unique group classes
+let scroller1 = initializeScroller('.scrollerContainer', 'scrollerGroup');
+let scroller2 = initializeScroller('.scrollerContainer-02', 'scrollerGroup-02');
+
+// On document ready
+$(document).ready(() => {
+  initializeResponsiveScroller();
+
+  // Debounce resize to avoid frequent updates
+  let resizeTimeout;
+  $(window).resize(() => {
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(() => initializeResponsiveScroller(), 200);
+  });
 });
-$('.scrollerContainer-02').html(scrollerItemsEl01);
-
-// Wrap items in a box that is as wide as the all the elements combined.
-// This prevents the items from wrapping if wider than the scroller width
-$('.scrollerContainer-02 > article').wrapAll('<div class="scrollerGroup-02" />');
-var scrollerCount01 = $('.scrollerContainer-02 .scrollerGroup-02 > article').length;
-var scrollerItemWidth01 = parseInt($('.scrollerContainer').css('width')) / scrollerItemsViewable;
-$('.scrollerContainer-02 .scrollerGroup-02 > article').css('width', scrollerItemWidth01 + 'px');
-$('.scrollerContainer-02 .scrollerGroup-02').css('width',scrollerCount01 * scrollerItemWidth01 + 'px');
-$('.scrollerContainer-02 .scrollerGroup-02 > article').css('transition', 'margin ' + scrollerTransitionSpeed + 's');
-
-// Set Starting Values
-var scrollerLeftMargin01 = '-' + scrollerItemWidth01 + 'px';
-var scrollerFirstItem01 = true;
-
-scrollerAnimate01(scrollerSpeed);
-
-function scrollerAnimate01(speed) {
-  setInterval(scrollerRotate01, speed * 1000);
-}
-
-function scrollerRotate01() {
-  if (scrollerFirstItem01) {
-    scrollerFirstItem01 = false;
-  } else {
-    $('.scrollerContainer-02 .scrollerGroup-02').append($('.scrollerContainer-02 .scrollerGroup-02 article:first-child'));
-  }
-  $('.scrollerContainer-02 .scrollerGroup-02 > article').css('margin-left', '0');
-  $('.scrollerContainer-02 .scrollerGroup-02 > article:first-child').css('margin-left', scrollerLeftMargin01);
-}
